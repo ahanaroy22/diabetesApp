710087b1f98e91f8dbe69cf73f16ef6b63e89753
ahanaroy22 <ahanaroy.chowdhury2012@vit.ac.in>
2017-03-06 15:59:10 +0530

    histogram for bloodpressure

--- R/bostonhist.R
+++ R/bostonhist.R
@@ -1,3 +1,10 @@
 bostonhist <- function(variable){
-	hist(Boston[[variable]], xlab=variable, main="Example plot with lazyload data")
+	# hist(Boston[[variable]], xlab=variable, main="Example plot with lazyload data")
+	mydata <- read.csv("D:\MAJOR PROJECT\diabetes.csv", header=TRUE)
+	hist(mydata,xlab="BloodPressure", border="blue", 
+     col="green", 
+     xlim=c(100,700), 
+     las=1, 
+     breaks=5, 
+     prob = TRUE)
 }
